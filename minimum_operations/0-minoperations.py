6f3cc3022b22967d05a33714a493be762c6e1862
Stephen30o0 <106529980+Stephen30o0@users.noreply.github.com>
2023-02-09 22:41:48 +0200

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python3
+"""Calculates the minimum number of Copy and Paste
+operations needed to get n H characters in a text file
+"""
 
+
+def minOperations(n):
+    """return 0 if n is impossible to achieve"""
+    if n <= 0:
+        return 0
+    
+    operations = 0
+    i = 2 # 
+    
+    """divide n by i as many times as possible"""
+    while i <= n:
+        while n % i == 0:
+            operations += i 
+            n = n // i 
+        i += 1 
+    
+    """if n is still greater than 1, add n as the final operation"""
+    if n > 1:
+        operations += n
+    
+    return operations 
