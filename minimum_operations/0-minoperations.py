6d4390726ecf1a54d05aef6bd0f3261b3227d4df
Stephen30o0 <106529980+Stephen30o0@users.noreply.github.com>
2023-02-09 22:07:48 +0200

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-
+"""
+This is a function that calculates the minimum number
+of operations needed to result in exactly n 'H' characters in a text file.
+"""
 def minOperations(n):
-    if n == 0:
+    """ return 0 if n is less than or equal to 0"""
+    if n <= 0:
         return 0
     operations = 0
     i = 2
-    while i * i <= n:
+    """ iterate through all possible divisors of n"""
+    while i <= n:
+        """add the divisor to operations count and divide n by it
+         until n is no longer divisible by it"""
         while n % i == 0:
             operations += i
             n = n // i
         i += 1
-    if n > 1:
-        operations += n
+    """ return the total number of operations"""
     return operations
+
